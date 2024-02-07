cbdf2f95b03c5657ac1872a997d4a69f9fef7153
Andrew <awoodcoc@indiana.edu>
2024-02-06 21:11:07 -0600

    Initialized Spring Boot

---
+++ src/main/java/com/awoodcock/armory/ArmoryApplication.java
@@ -0,0 +1,13 @@
+package com.awoodcock.armory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmoryApplication.class, args);
+	}
+
+}
