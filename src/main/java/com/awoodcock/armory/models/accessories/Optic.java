b7229191e9630f49bf1333f395cbe3935774573d
Andrew <awoodcoc@indiana.edu>
2024-03-11 18:31:51 -0500

    Completed initial drafting of POJOs

--- src/main/java/com/awoodcock/armory/models/accessories/Optic.java
+++ src/main/java/com/awoodcock/armory/models/accessories/Optic.java
@@ -5,9 +5,34 @@ import com.awoodcock.armory.models.AbstractEntity;
 public class Optic extends AbstractEntity {
 
     // PROPERTIES
+    private String classification;
+
+    private int magnification;
 
     // CONSTRUCTORS
+    public Optic(){}
+
+    public Optic(String classification, int magnification) {
+        super();
+        this.classification = classification;
+        this.magnification = magnification;
+    }
+
 
     // GETTERS AND SETTERS
+    public String getClassification() {
+        return classification;
+    }
 
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public int getMagnification() {
+        return magnification;
+    }
+
+    public void setMagnification(int magnification) {
+        this.magnification = magnification;
+    }
 }
