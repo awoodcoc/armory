5c8f65c7e374bdb592f69c8bfdd89d288f1a6a0e
Andrew <awoodcoc@indiana.edu>
2024-06-03 20:57:51 -0500

    Begin set up to update / refactor AR class

--- src/main/java/com/awoodcock/armory/service/ar/ARService.java
+++ src/main/java/com/awoodcock/armory/service/ar/ARService.java
@@ -1,7 +1,9 @@
 package com.awoodcock.armory.service.ar;
 
-import com.awoodcock.armory.data.ar.ARRepository;
-import com.awoodcock.armory.models.ar.AR;
+import com.awoodcock.armory.data.accessories.MagazineRepository;
+import com.awoodcock.armory.data.accessories.OpticRepository;
+import com.awoodcock.armory.data.accessories.SlingRepository;
+import com.awoodcock.armory.data.ar.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,54 @@ public class ARService {
     // REPOSITORY
     private final ARRepository arRepository;
 
+    // AR
+    private final BarrelRepository barrelRepository;
+
+    private final BCGRepository bcgRepository;
+
+    private final ChargingHandleRepository chargingHandleRepository;
+
+    private final HandguardRepository handguardRepository;
+
+    private final LowerRepository lowerRepository;
+
+    private final PistolGripRepository pistolGripRepository;
+
+    private final StockRepository stockRepository;
+
+    private final TrigRepository trigRepository;
+
+    private final UpperRepository upperRepository;
+
+    // Accessories
+    private final MagazineRepository magazineRepository;
+
+    private final OpticRepository opticRepository;
+
+    private final SlingRepository slingRepository;
+
     // CONSTRUCTOR
     @Autowired
-    public ARService (ARRepository arRepository) {
+    public ARService (ARRepository arRepository, BarrelRepository barrelRepository,
+                      BCGRepository bcgRepository, ChargingHandleRepository chargingHandleRepository,
+                      HandguardRepository handguardRepository, LowerRepository lowerRepository,
+                      PistolGripRepository pistolGripRepository, StockRepository stockRepository,
+                      TrigRepository trigRepository, UpperRepository upperRepository,
+                      MagazineRepository magazineRepository, OpticRepository opticRepository,
+                      SlingRepository slingRepository) {
         this.arRepository = arRepository;
+        this.barrelRepository = barrelRepository;
+        this.bcgRepository = bcgRepository;
+        this.chargingHandleRepository = chargingHandleRepository;
+        this.handguardRepository = handguardRepository;
+        this.lowerRepository = lowerRepository;
+        this.pistolGripRepository = pistolGripRepository;
+        this.stockRepository = stockRepository;
+        this.trigRepository = trigRepository;
+        this.upperRepository = upperRepository;
+        this.magazineRepository = magazineRepository;
+        this.opticRepository = opticRepository;
+        this.slingRepository = slingRepository;
     }
 
     // CRUD OPERATIONS for AR class
