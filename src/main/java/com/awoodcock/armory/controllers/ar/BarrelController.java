518988a535df8db7b7cc8ccfb80926bd0937d06f
Andrew <awoodcoc@indiana.edu>
2024-08-20 18:01:59 -0500

    initial commit on controller classes. Begin infrastructure with AR and Barrel controllers

---
+++ src/main/java/com/awoodcock/armory/controllers/ar/BarrelController.java
@@ -0,0 +1,12 @@
+package com.awoodcock.armory.controllers.ar;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("barrels")
+@CrossOrigin
+public class BarrelController {
+
+}
